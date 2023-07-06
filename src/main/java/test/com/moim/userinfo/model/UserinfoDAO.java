7c33cbe1337bd44f76179736d63f3a0b34bc0a2c
nayeon <wendy3302@naver.com>
2023-07-06 14:15:22 +0900

    nayeon

---
+++ src/main/java/test/com/moim/userinfo/model/UserinfoDAO.java
@@ -0,0 +1,17 @@
+package test.com.moim.userinfo.model;
+
+import java.util.List;
+
+public interface UserinfoDAO {
+
+	List<UserinfoVO> selectAll();
+
+	UserinfoVO selectOne(UserinfoVO vo);
+
+	int insert(UserinfoVO vo);
+
+	UserinfoVO login(UserinfoVO vo);
+
+	UserinfoVO idCheck(UserinfoVO vo);
+
+}
