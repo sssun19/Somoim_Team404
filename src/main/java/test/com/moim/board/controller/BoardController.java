ae5fb07045bea2bcf9164d83f9296b6192c34230
BeomV <101873654+BeomV@users.noreply.github.com>
2023-07-06 16:43:19 +0900

    BeomJun Commit

--- src/main/java/test/com/moim/board/controller/BoardController.java
+++ src/main/java/test/com/moim/board/controller/BoardController.java
@@ -31,6 +31,55 @@ public class BoardController {
     HttpSession session;
 
 
+//    @RequestMapping(value = "/som_selectAll.do", method = RequestMethod.GET)
+//    public String som_selectAll(Model model) {
+//        log.info("som_selectAll.do().....{}");
+//
+//        List<BoardVO> vos = service.selectAll();
+//
+//        for (BoardVO vo : vos) {
+//            log.info(vo.toString());
+//        }
+//
+//        model.addAttribute("vos",vos);
+//
+//        return "board/som_selectAll";
+//    }
+//
+//    @RequestMapping(value = "/som_insert.do", method = RequestMethod.GET)
+//    public String som_insert() {
+//        log.info("som_insert.do().....");
+//
+//
+//        return "board/som_insert";
+//    }
+//
+//    @RequestMapping(value = "/som_insertOK.do", method = RequestMethod.POST)
+//    public String som_insertOK(BoardVO vo) {
+//        log.info("som_insertOK.do().....");
+//
+//        int result = service.insert(vo);
+//        log.info("insertOK...{}",vo);
+//        return "board/som_insert";
+//    }
+//
+//    @RequestMapping(value = "/som_selectOne.do", method = RequestMethod.GET)
+//    public String som_selectOne(BoardVO vo, Model model) {
+//        log.info("som_selectOne.do().....");
+//
+//        BoardVO vo2 = service.selectOne(vo);
+//        log.info("test...{}",vo2);
+//
+//        model.addAttribute("vo2",vo2);
+//
+//        session.setAttribute("num",vo.getNum());
+//        session.setAttribute("Board_num", vo.getNum());
+//        log.info(String.valueOf(vo.getNum()));
+//
+//
+//        return "board/som_selectOne";
+//    }
+
     @RequestMapping(value = "/join_selectAll.do", method = RequestMethod.GET)
     public String join_selectAll(Model model,Somoim_BoardVO vo) {
         log.info("join_selectAll().....");
