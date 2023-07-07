f3996ef590017bd94830d8cc393b89744fb5188a
sssun <98_0509@naver.com>
2023-07-07 18:43:03 +0900

    ccommmiiitt

--- src/main/java/test/com/moim/mypage/controller/MypageController.java
+++ src/main/java/test/com/moim/mypage/controller/MypageController.java
@@ -1,23 +1,50 @@
 package test.com.moim.mypage.controller;
 
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
+import test.com.moim.mypage.service.MypageService;
+import test.com.moim.userinfo.model.UserinfoVO;
 
 
 @Slf4j
 @Controller
 public class MypageController {
+	
+	@Autowired
+	HttpSession session;
+	
+	@Autowired
+	MypageService service;
 
 	@RequestMapping(value = "/mypage.do", method = RequestMethod.GET)
-	public String home() {
-		log.info("home.do().....");
-
+	public String home(Model model) {
+		log.info("mypage open....");
+		UserinfoVO vo = new UserinfoVO();
+		String user_id = (String)session.getAttribute("user_id");
+		vo.setUser_id(user_id);
+		log.info(vo.getUser_id());
 		
+		UserinfoVO vo2 = service.mypageSelectOne(vo);
+		
+		log.info(vo2.getSave_name());
+		log.info(vo2.getName());
+		log.info(vo2.getEmail());
+		log.info(vo2.getBirthday());
+		
+		String mypageBirth = vo2.getBirthday().substring(0, 10);
+		log.info(mypageBirth);
+		
+		vo2.setBirthday(mypageBirth);
+
+		model.addAttribute("vo2", vo2);
 
 		return "mypage/mypage";
 	}
