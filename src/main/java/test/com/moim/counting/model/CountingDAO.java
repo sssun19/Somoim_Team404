99b82de65f0f0b01b03ca8f254e95006f677cd85
HyungJu <ccmchanggo@gmail.com>
2023-07-12 05:07:22 +0900

    1234

---
+++ src/main/java/test/com/moim/counting/model/CountingDAO.java
@@ -0,0 +1,9 @@
+package test.com.moim.counting.model;
+
+public interface CountingDAO {
+    public void good_count_up();
+    public void good_count_down();
+    public void v_count_up();
+    public void v_count_down();
+    }
+
---
+++ src/main/java/test/com/moim/counting/model/CountingVO.java
@@ -0,0 +1,9 @@
+package test.com.moim.counting.model;
+
+import lombok.Data;
+
+@Data
+public class CountingVO {
+    private int  view_count;
+    private int good_count;
+}
