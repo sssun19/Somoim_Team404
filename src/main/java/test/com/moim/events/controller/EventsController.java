4bec7982d77cd4ab654162cfb7e011cd50e2ad03
sssun <98_0509@naver.com>
2023-07-13 13:17:54 +0900

    commit

--- src/main/java/test/com/moim/events/controller/EventsController.java
+++ src/main/java/test/com/moim/events/controller/EventsController.java
@@ -17,136 +17,132 @@ import java.util.List;
 @Controller
 public class EventsController {
 
-    @Autowired
-    EventsService service;
+	@Autowired
+	EventsService service;
 
-    @Autowired
-    HttpSession session;
+	@Autowired
+	HttpSession session;
 
-    @RequestMapping(value = "/events_main.do", method = RequestMethod.GET)
-    public String events_main() {
+	@RequestMapping(value = "/events_main.do", method = RequestMethod.GET)
+	public String events_main() {
 
-        log.info("events_main.do()......");
+		log.info("events_main.do()......");
 
-        return "event/event_List";
-    }
-    @RequestMapping(value = "/eventSelectAll.do", method = RequestMethod.GET)
-    public String eventSelectAll(Model model){
-        String userId = (String) session.getAttribute("user_id");
+		return "event/event_List";
+	}
 
-        model.addAttribute("user_id", userId);
-        log.info("userId..{}", userId);
+	@RequestMapping(value = "/eventSelectAll.do", method = RequestMethod.GET)
+	public String eventSelectAll(Model model) {
+		String userId = (String) session.getAttribute("user_id");
 
-        List<EventsVO> vos = service.selectAll();
-        log.info("vos...{}", vos);
-        model.addAttribute("vos", vos);
-        return "event/event_List";
-    }
+		model.addAttribute("user_id", userId);
+		log.info("userId..{}", userId);
 
-    @RequestMapping(value = "/eventsSelctOne.do", method = RequestMethod.GET)
-    public String eventsSelectOne(EventsVO vo, Model model){
-        log.info("EvnetsVO...{}", vo);
-        EventsVO vo2 = service.selectOne(vo);
-        log.info("vo2...{}", vo2);
-        model.addAttribute("vo2", vo2);
-        return "event/event";
-    }
+		List<EventsVO> vos = service.selectAll();
+		log.info("vos...{}", vos);
+		model.addAttribute("vos", vos);
+		return "event/event_List";
+	}
 
-    @RequestMapping(value = "/InsertEvents.do", method = RequestMethod.GET)
-    public String InsertEvents(EventsVO vo, Model model){
-        String userId = (String) session.getAttribute("user_id");
+	@RequestMapping(value = "/eventsSelctOne.do", method = RequestMethod.GET)
+	public String eventsSelectOne(EventsVO vo, Model model) {
+		log.info("EvnetsVO...{}", vo);
+		EventsVO vo2 = service.selectOne(vo);
+		log.info("vo2...{}", vo2);
+		model.addAttribute("vo2", vo2);
+		return "event/event";
+	}
 
-        model.addAttribute("user_id", userId);
-        log.info("userId..{}", userId);
+	@RequestMapping(value = "/InsertEvents.do", method = RequestMethod.GET)
+	public String InsertEvents(EventsVO vo, Model model) {
+		String userId = (String) session.getAttribute("user_id");
 
+		model.addAttribute("user_id", userId);
+		log.info("userId..{}", userId);
 
-        return "event/InsertEvents";
-    }
+		return "event/InsertEvents";
+	}
 
+	@RequestMapping(value = "/InsertOKEvents.do", method = RequestMethod.POST)
+	public String InsertOKEvents(EventsVO vo, Model model) {
+		String userId = (String) session.getAttribute("user_id");
 
-    @RequestMapping(value = "/InsertOKEvents.do", method = RequestMethod.POST)
-    public String InsertOKEvents(EventsVO vo, Model model){
-        String userId = (String) session.getAttribute("user_id");
+		model.addAttribute("user_id", userId);
+		log.info("userId..{}", userId);
+		log.info("vovovovovovovovovo..{}", vo);
+		int result = service.insert(vo);
 
-        model.addAttribute("user_id", userId);
-        log.info("userId..{}", userId);
-        log.info("vovovovovovovovovo..{}", vo);
-        int result = service.insert(vo);
+		if (result == 1) {
+			log.info("작성완 ㅋ");
+			return "redirect:eventSelectAll.do";
 
-        if(result==1){
-            log.info("작성완 ㅋ");
-            return "redirect:eventSelectAll.do";
+		} else {
+			return "redirect:InsertEvents.do";
+		}
 
-        }else{
-            return "redirect:InsertEvents.do";
-        }
+	}
 
-    }
-    @RequestMapping(value = "/updateEvents.do", method = RequestMethod.GET)
-    public String updateEvents(EventsVO vo, Model model){
-        log.info("update********* vo..{}", vo);
-        String userId = (String) session.getAttribute("user_id");
-        model.addAttribute("user_id", userId);
+	@RequestMapping(value = "/updateEvents.do", method = RequestMethod.GET)
+	public String updateEvents(EventsVO vo, Model model) {
+		log.info("update********* vo..{}", vo);
+		String userId = (String) session.getAttribute("user_id");
+		model.addAttribute("user_id", userId);
 
-        log.info("userId..{}", userId);
+		log.info("userId..{}", userId);
 
-        EventsVO vo2 =new EventsVO();
-        vo2 = service.selectOne(vo);
-model.addAttribute("vo2", vo2);
-log.info("new vo2!!!!!!!!!!..{}", vo2);
+		EventsVO vo2 = new EventsVO();
+		vo2 = service.selectOne(vo);
+		model.addAttribute("vo2", vo2);
+		log.info("new vo2!!!!!!!!!!..{}", vo2);
 
-        return "event/updateEvents";
-    }
+		return "event/updateEvents";
+	}
 
+	@RequestMapping(value = "/updateOKEvents.do", method = RequestMethod.GET)
+	public String updateOKEvents(EventsVO vo, Model model) {
+		String userId = (String) session.getAttribute("user_id");
 
+		model.addAttribute("user_id", userId);
+		log.info("userId..{}", userId);
+		log.info("update vovovovo..{}", vo);
 
-    @RequestMapping(value = "/updateOKEvents.do", method = RequestMethod.GET)
-    public String updateOKEvents(EventsVO vo, Model model){
-        String userId = (String) session.getAttribute("user_id");
+		EventsVO vo2 = new EventsVO();
+		vo2.setNum(vo.getNum());
+		vo2 = service.selectOne(vo2);
+		log.info("new vo2 vo2 vo2..{}", vo2);
+		if (vo.getSave_name() == null || vo.getSave_name().equals("")) {
+			vo.setSave_name(vo2.getSave_name());
+		}
+		log.info("vo..{}", vo);
+		int result = service.update(vo);
 
-        model.addAttribute("user_id", userId);
-        log.info("userId..{}", userId);
-        log.info("update vovovovo..{}", vo);
+		if (result == 1) {
+			log.info("작성완 ㅋ");
+			return "redirect:eventSelectAll.do";
 
-        EventsVO vo2 = new EventsVO();
-        vo2.setNum(vo.getNum());
-        vo2 = service.selectOne(vo2);
-        log.info("new vo2 vo2 vo2..{}",vo2 );
-        if (vo.getSave_name() == null || vo.getSave_name().equals("")) {
-            vo.setSave_name(vo2.getSave_name());
-        }
-log.info("vo..{}", vo);
-        int result = service.update(vo);
+		} else {
+			return "redirect:updateEvents.do?num=" + vo.getNum();
+		}
 
-        if(result==1){
-            log.info("작성완 ㅋ");
-            return "redirect:eventSelectAll.do";
+	}
 
-        }else{
-            return "redirect:updateEvents.do?num="+vo.getNum();
-        }
+	@RequestMapping(value = "/deleteOKEvents.do", method = RequestMethod.GET)
+	public String deleteOKEvents(EventsVO vo, Model model) {
+		String userId = (String) session.getAttribute("user_id");
 
-    }
+		model.addAttribute("user_id", userId);
+		log.info("userId..{}", userId);
+		log.info("update vovovovo..{}", vo);
+		int result = service.delete(vo);
 
-    @RequestMapping(value = "/deleteOKEvents.do", method = RequestMethod.GET)
-    public String deleteOKEvents(EventsVO vo, Model model){
-        String userId = (String) session.getAttribute("user_id");
+		if (result == 1) {
+			log.info("삭제완 ㅋ");
+			return "redirect:eventSelectAll.do";
 
-        model.addAttribute("user_id", userId);
-        log.info("userId..{}", userId);
-        log.info("update vovovovo..{}", vo);
-        int result = service.delete(vo);
+		} else {
+			return "redirect:eventSelectAll.do?num=" + vo.getNum();
+		}
 
-        if(result==1){
-            log.info("삭제완 ㅋ");
-            return "redirect:eventSelectAll.do";
-
-        }else{
-            return "redirect:eventSelectAll.do?num="+vo.getNum();
-        }
-
-    }
+	}
 
 }
-
-
