93ee1b87093334915134e41525d8f3cfb163ce7e
sssun <98_0509@naver.com>
2023-07-14 17:45:51 +0900

    commit

--- src/main/java/test/com/moim/myfeed/controller/MyfeedController.java
+++ src/main/java/test/com/moim/myfeed/controller/MyfeedController.java
@@ -5,10 +5,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.extern.slf4j.Slf4j;
+import test.com.moim.mypage.service.MypageService;
 import test.com.moim.userinfo.model.UserinfoVO;
 
 
@@ -20,40 +22,22 @@ public class MyfeedController {
 	@Autowired
 	HttpSession session;
 
+	@Autowired
+	MypageService mypageService;
+	
 	@RequestMapping(value = "/myfeed_feed_mine.do", method = RequestMethod.GET)
-	public String myfeed_feed_mine() {
+	public String myfeed_feed_mine(Model model) {
 		log.info("myfeed_feed_mine.do().....");
 		String user_id = (String) session.getAttribute("user_id");
 		UserinfoVO vo = new UserinfoVO();
 		vo.setUser_id(user_id);
-
 		log.info("id?? : {}", vo.getUser_id());
+		
+		UserinfoVO vo2 = mypageService.mypageSelectOne(vo);
 
+		model.addAttribute("vo2", vo2);
 		return "myfeed/myfeed_feed_mine";
 	}
-	
-	@RequestMapping(value = "/myfeed_feed.do", method = RequestMethod.GET)
-	public String myfeed_feed() {
-		log.info("myfeed_feed.do().....");
-//		String user_id = (String) session.getAttribute("user_id");
-//		UserinfoVO vo = new UserinfoVO();
-//		vo.setUser_id(user_id);
-//		
-//		log.info("id?? : {}", vo.getUser_id());
-		
-		return "myfeed/myfeed_feed";
-	}
-	
-	@RequestMapping(value = "/myfeed_moim.do", method = RequestMethod.GET)
-	public String myfeed_moim() {
-		log.info("myfeed_moim.do().....");
-//		String user_id = (String) session.getAttribute("user_id");
-//		UserinfoVO vo = new UserinfoVO();
-//		vo.setUser_id(user_id);
-//		
-//		log.info("id?? : {}", vo.getUser_id());
-		
-		return "myfeed/myfeed_moim";
-	}
+
 	
 }
